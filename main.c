960382215ebce584722f81c24513e0962aa6935d
chr1swill <alexanderwilliams20003@gmail.com>
2024-08-01 12:21:29 -0400

    feat: add division command

--- main.c
+++ main.c
@@ -16,9 +16,10 @@ print_help_message:
         puts("No input was provided to process\n");
         puts("Syntax: <command> [...parameters]\n");
         puts("Accepted commands: \n");
-        puts("  +   [addition] ..................... returns the sum of the provided parameters.\n");
-        puts("  -   [subtraction] .................. returns the difference of the provided parameters.\n");
-        puts("  x   [multiplication] ............... returns the product of the provided parameters.\n");
+        puts("  [+] - addition ..................... returns the sum.\n");
+        puts("  [-] - subtraction .................. returns the difference.\n");
+        puts("  [x] - multiplication ............... returns the product.\n");
+        puts("  [/] - division ..................... returns the quotient.\n");
         puts("\n");
         puts("Example:\n");
         printf("Input: %s + 10 10\n", PROGRAM_NAME);
@@ -87,6 +88,27 @@ print_help_message:
                 int i = INDEX_INTO_ARGV_PARAM_TWO;
                 while (i < argc) {
                     total *= atof(argv[i]);
+                    i++;
+                };
+
+                printf("%f\n", total);
+                return 0;
+            };
+
+            break;
+        case '/': 
+            if (argc == ARGC_WITH_NO_PARAMS) goto print_help_message;
+
+            if (argc == ARGC_WITH_ONE_PARAM) {
+                double param_one = atof(argv[INDEX_INTO_ARGV_PARAM_ONE]);
+                printf("%f\n", param_one);
+                return 0;
+            } else {
+                double total = atof(argv[INDEX_INTO_ARGV_PARAM_ONE]);
+
+                int i = INDEX_INTO_ARGV_PARAM_TWO;
+                while (i < argc) {
+                    total /= atof(argv[i]);
                     i++;
                 };
 
